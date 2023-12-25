3d997929bf358da9d842a7a439bb81de35cfe460
bhlvoong <bhlvoong@gmail.com>
2023-12-24 22:32:20 -0800

    Initial Commit

---
+++ AppStoreMockInterview/AppStoreMockInterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreMockInterviewApp.swift
+//  AppStoreMockInterview
+//
+//  Created by Brian Voong on 12/24/23.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreMockInterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
